ca25801e538069fd94bc8d54ddfa5daf61232b0f
MeAsMehrun <motamednia2003@gmail.com>
2024-09-10 14:45:15 +0330

    Third Commit

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-class BlogModels:
-    # detail =
-    # youtube_Title =
-    # youtube_link = 
-    pass
+class BlogModels(models.Model):
+    title = models.CharField(max_length=500)
+    detail = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    youtube_Title = models.CharField(max_length=500)
+    youtube_link = models.URLField(blank=True)
+
+
+    def __str__(self):
+        return self.title
